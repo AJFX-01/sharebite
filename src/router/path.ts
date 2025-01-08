7d5fc11cb5b7c37164159d40bd52cd5c1670e7bb
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2025-01-09 00:37:44 +0100

    feat: added new parts

--- src/router/path.ts
+++ src/router/path.ts
@@ -2,14 +2,14 @@ export const rootPaths = {
   root: '/',
   pageRoots: '/',
   authRoot: '/',
-  errorRoot: '/error'
+  errorRoot: '/error',
 };
-
 
 const paths = {
   default: `${rootPaths.root}`,
   login: `${rootPaths.authRoot}login`,
-  signup: `${rootPaths.authRoot}sign-up`
+  signup: `${rootPaths.authRoot}sign-up`,
+  notfound: `${rootPaths.errorRoot}/404`,
 };
 
 export default paths;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,8 +2,6 @@ import { lazy, Suspense } from 'react';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import paths, { rootPaths } from './path';
 
-
-
 /* ---------------- Lazy loads various components ------------------------- */
 const App = lazy(() => import('App'));
 const MainLayout = lazy(() => import('layouts/main-layout'));
@@ -12,24 +10,25 @@ const AuthLayout = lazy(() => import('layouts/auth-layout'));
 const SignupPage = lazy(() => import('pages/authentication/signup'));
 const LoginPage = lazy(() => import('pages/authentication/login'));
 
-
 const Spinner = lazy(() => import('components/loading/Spinner'));
-const LoadingProgress = lazy(() => import('components/loading/LoadingProgress'));
+const LoadingProgress = lazy(
+  () => import('components/loading/LoadingProgress'),
+);
+
+const NotFoundPage = lazy(() => import('pages/not-found'));
 /* -------------------------------------------------------------------------- */
-
-
 
 export const routes = [
   {
     elements: (
-      <Suspense fallback={<Spinner/>}>
+      <Suspense fallback={<Spinner />}>
         <App />
       </Suspense>
     ),
     children: [
       {
         path: paths.default,
-        element: <Navigate to={paths.login} replace />
+        element: <Navigate to={paths.login} replace />,
       },
       {
         path: rootPaths.authRoot,
@@ -37,18 +36,30 @@ export const routes = [
         children: [
           {
             path: paths.login,
-            element: <LoginPage />
+            element: <LoginPage />,
           },
           {
             path: paths.signup,
-            element: <SignupPage />
-          }
-        ]
-      }
-    ]
-  }
+            element: <SignupPage />,
+          },
+        ],
+      },
+      {
+        path: rootPaths.errorRoot,
+        children: [
+          {
+            path: paths.notfound,
+            element: <NotFoundPage />,
+          },
+        ],
+      },
+      {
+        path: '*',
+        element: <Navigate to={paths.notfound} replace />,
+      },
+    ],
+  },
 ];
-
 
 const router = createBrowserRouter(routes, {
   basename: '/',
