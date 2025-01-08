a2d238a523d130f226b7b3a249f0d991e62a7949
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2025-01-08 14:45:45 +0100

    feat: added few routes and spinner commponnet

---
+++ src/pages/authentication/signup/index.tsx
@@ -0,0 +1,11 @@
+import SignupForm from "components/common/Signup";
+
+const SignupPage = () => {
+    return (
+      <>
+        <SignupForm />
+      </>
+    )
+}
+
+export default SignupPage;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,3 +1,28 @@
 import { lazy, Suspense } from 'react';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import paths, { rootPaths } from './path';
+import App from 'App';
+
+
+/* ---------------- Lazy loads various components ------------------------- */
+const SignupPage = lazy(() => import('pages/authentication/signup'));
+const LoginPage = lazy(() => import('pages/authentication/login'));
+/* -------------------------------------------------------------------------- */
+
+
+
+export const routes = [
+  {
+    elements: (
+      <Suspense fallback={}>
+        <App />
+      </Suspense>
+    ),
+    children: [
+      {
+        path: paths.default,
+        element: <Navigate to={paths.login} replace />
+      },
+    ]
+  }
+]
