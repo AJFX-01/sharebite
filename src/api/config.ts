490129d1db55ed032abb832e89b80ef4d7589666
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2025-01-09 02:38:19 +0100

    completed axios basic instamce

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
+import paths from 'router/path';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -6,5 +7,29 @@ const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
-  }
+  },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accesToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401 && error.response) {
+      // If we receive a 401 error, clear the token and redirect to login
+      localStorage.removeItem('accessToken');
+      window.location.href = paths.login;
+    }
+  },
+);
