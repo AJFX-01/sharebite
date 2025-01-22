c6f622960f85907355665dc195d7b4a47f7df8ed
ajfx-01 <opeyemi.ajegbomogun@yahoo.com>
2025-01-22 04:04:07 +0100

    feat: persist users

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const UserContext = createContext<UserContext | undefined>(undefined);
 
@@ -13,7 +13,18 @@ export const useUser = () => {
 };
 
 export const UserProvider = ({ children }: ContextProps) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(() => {
+    const savedUser = localStorage.getItem('user');
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      localStorage.removeItem('user');
+    }
+  }, [user]);
 
   const isAuthenticated = Boolean(user);
   const login = (user: User) => setUser(user);
